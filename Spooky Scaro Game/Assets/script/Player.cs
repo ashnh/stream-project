587a5f08f709cdd997b4b2f4799173f0a6d3582f
ashnhx <ashnhx@gmail.com>
2017-06-10 10:51:50 -0400

    initial commit

---
+++ Spooky Scaro Game/Assets/script/Player.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public GameObject cameraObject;
+
+	public float speed;
+	public float jump;
+
+	float zFactor;
+	float xFactor;
+
+	void controls () {
+
+		if (PlayerPrefs.GetInt ("stop") == 1) {
+			if (Input.GetKey (KeyCode.W)) {
+				zFactor = 1f;
+			} else if (Input.GetKey (KeyCode.S)) {
+				zFactor = -1f;
+			} else {
+				zFactor = 0f;
+			}
+
+			if (Input.GetKey (KeyCode.D)) {
+				xFactor = 1f;
+			} else if (Input.GetKey (KeyCode.A)) {
+				xFactor = -1f;
+			} else {
+				xFactor = 0f;
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			if (PlayerPrefs.GetInt ("stop") == 1) {
+				PlayerPrefs.SetInt ("stop", 0);
+			} else {
+				PlayerPrefs.SetInt ("stop", 1);
+			}
+		}
+			
+		GetComponent <Rigidbody> ().velocity = cameraObject.transform.TransformDirection ( new Vector3 (speed * xFactor, GetComponent <Rigidbody> ().velocity.y, speed * zFactor) );
+
+
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent <Rigidbody> ().velocity = new Vector3 (GetComponent <Rigidbody> ().velocity.x, jump, GetComponent <Rigidbody> ().velocity.z);
+		}
+
+
+	}
+
+	void cameraControl () {
+
+		cameraObject.transform.position = transform.position; //+ new Vector3 (0, 10, -10);
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			cameraObject.transform.Rotate (new Vector3 (0f, -2f, 0f));
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			cameraObject.transform.Rotate (new Vector3 (0f, 2f, 0f));
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		PlayerPrefs.SetInt ("stop", 0);
+	
+		zFactor = 0f;
+		xFactor = 0f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		cameraControl ();
+
+		controls ();
+
+	}
+}
---
+++ Spooky Scaro Game/Assets/script/StopableObject.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopableObject : MonoBehaviour {
+
+
+
+	// user inputed values
+	public string routine;
+	public string pairedValues;
+	public bool repeat;
+
+
+
+	// stage movement
+	string currentStage;
+	int currentStageNumber;
+	bool inProgress;
+	Vector3 startPosition;
+	string[] sequence () {
+		return routine.Split (';');
+	}
+	string[] pairedSequence () {
+		return pairedValues.Split (';');
+	}
+	void moveSequence () {
+		if (sequence ().Length - 1 >= currentStageNumber) {
+			currentStageNumber++;
+			currentStage = sequence () [currentStageNumber];
+		}
+	}
+	void resetSequence () {
+		currentStageNumber = 0;
+		currentStage = sequence () [currentStageNumber];
+	}
+
+
+
+	// stages
+	bool moveForward (string stage, float edge, float startPosition, float speed) {
+		bool isOver;
+		if (speed >= 0) {
+			isOver = /*over condition*/ !(transform.position.z < edge + startPosition);
+		} else {
+			isOver = /*over condition*/ !(transform.position.z > edge + startPosition);
+		}
+		if (stage == "moveForward") {
+			if (!isOver) {
+			// sequence
+				GetComponent <Rigidbody> ().velocity = new Vector3 (GetComponent <Rigidbody> ().velocity.x, GetComponent <Rigidbody> ().velocity.y, speed);
+				inProgress = true;
+			} else {
+				inProgress = false;
+				GetComponent <Rigidbody> ().velocity = new Vector3 (0, 0, 0);
+			}
+		}
+		return isOver;
+	}
+	bool moveSideways (string stage, float edge, float startPosition, float speed) {
+		bool isOver;
+		if (speed >= 0) {
+			isOver = /*over condition*/ !(transform.position.x < edge + startPosition);
+		} else {
+			isOver = /*over condition*/ !(transform.position.x > edge + startPosition);
+		}
+		if (stage == "moveSideways") {
+			if (!isOver) {
+				// sequence
+				GetComponent <Rigidbody> ().velocity = new Vector3 (speed, GetComponent <Rigidbody> ().velocity.y, GetComponent <Rigidbody> ().velocity.z);
+				inProgress = true;
+			} else {
+				inProgress = false;
+				GetComponent <Rigidbody> ().velocity = new Vector3 (0, 0, 0);
+			}
+		}
+		return isOver;
+	}
+
+
+
+	// routine execution
+	void processSteps () {
+		if (!inProgress) {
+			startPosition = transform.position;
+		}
+		switch (currentStage) {
+		case "moveForward":
+			if (moveForward (currentStage, float.Parse(pairedSequence ()[currentStageNumber].Split (',')[0]) , startPosition.z, float.Parse(pairedSequence ()[currentStageNumber].Split (',')[1]))) {
+				moveSequence ();
+				GetComponent <Rigidbody> ().velocity = new Vector3 (0, 0, 0);
+			}
+			break;
+		case "moveSideways":
+			if (moveSideways (currentStage, float.Parse(pairedSequence ()[currentStageNumber].Split (',')[0]) , startPosition.z, float.Parse(pairedSequence ()[currentStageNumber].Split (',')[1]))) {
+				moveSequence ();
+				GetComponent <Rigidbody> ().velocity = new Vector3 (0, 0, 0);
+			}
+			break;
+		case "stop":
+		default:
+			if (!repeat) {
+				if (!inProgress) {
+					GetComponent <Rigidbody> ().velocity = new Vector3 (0, 0, 0);
+					inProgress = true;
+				}
+			} else {
+				resetSequence ();
+			}
+			break;
+		}
+		Debug.Log (currentStage);
+	}
+
+
+
+	// initialization
+	void Start () {
+		currentStageNumber = 0;
+		currentStage = sequence () [currentStageNumber];
+		inProgress = false;
+	}
+
+
+	
+	// routine execution call
+	void Update () {
+		if (PlayerPrefs.GetInt ("stop") == 0) {
+			processSteps ();
+		}
+	}
+
+
+}
+
+
